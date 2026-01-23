62475321f07d631f547e79f9b987a8339fa7f508
Your Name <you@example.com>
2026-01-23 07:39:07 +0100

    made some changes

--- app.js
+++ app.js
@@ -10,56 +10,37 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const port = new SerialPort({ path: 'COM3', baudRate: 9600 });
+const port = new SerialPort({ path: 'COM4', baudRate: 9600 });
 const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }));
 
-let discKleur = null;
-
 parser.on('data', (incoming) => {
-  incoming = incoming.trim();
-  console.log('SERIAL:', incoming); // prints incomming data to the terminal
+  incoming = incoming.trim().toLowerCase();
+  console.log('SERIAL:', incoming);
 
-  // first data send from arduino (color)
-  if (!discKleur) {
-    discKleur = incoming.toLowerCase();
-    return;
-  }
+  if (!incoming.includes(',')) return;
 
-  // second data send from arduino (number)
-  const nummer = parseInt(incoming, 10);
+  const parts = incoming.split(',');
 
-  if (isNaN(nummer)) {
-    discKleur = null;
-    return;
-  }
+  if (parts.length !== 2) return;
+
+  const kleur = parts[0];
+  const nummer = parseInt(parts[1], 10);
+
+  if (!['red', 'yellow'].includes(kleur)) return;
+  if (isNaN(nummer) || nummer < 1 || nummer > 3) return;
 
   const index = nummer - 1;
 
-  // Validatie
-  if (
-    !json.discVideoInfo[discKleur] ||
-    !json.discVideoLink[discKleur] ||
-    !json.discInfo[discKleur] ||
-    index < 0 ||
-    index > 4
-  ) {
-    discKleur = null;
-    return;
-  }
+  io.emit('discData', {
+    kleur,
+    nummer,
+    videoDescription: json.videoDescription[kleur][index],
+    videoTitle: json.videoTitle[kleur][index],
+    discInfo: json.discInfo[kleur],
+    discImg: json.discImg[kleur],
+    discVideoLink: json.videoLink[kleur][index]
 
-  const description = json.discVideoInfo[discKleur][index]; //gets data from discVideoInfo out of json
-  const videoLink = json.discVideoLink[discKleur][index];
-  const discInfo = json.discInfo[discKleur];
-
-io.emit('discData', {
-    kleur: discKleur,
-    nummer: nummer,
-    description: description,
-    videoLink: videoLink,
-    discInfo: discInfo
   });
-
-  discKleur = null;
 });
 
 app.use(express.static('site'));
